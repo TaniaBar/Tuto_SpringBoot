4646a50773b3c4c4972349c249ced8168ed84553
TaniaBar <tania.barillaro1@gmail.com>
2025-02-26 19:39:48 +0100

    create ClientService

---
+++ src/main/java/com/example/tuto/service/ClientService.java
@@ -0,0 +1,13 @@
+package com.example.tuto.service;
+
+import com.example.tuto.entities.Client;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@Service
+public class ClientService {
+
+    public void creer(Client client) {
+
+    }
+}
