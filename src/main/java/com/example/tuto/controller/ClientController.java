cdeb1f5cde7574bc65291c59195ebc358e3e85ff
TaniaBar <tania.barillaro1@gmail.com>
2025-02-26 19:56:33 +0100

    injection instances of differents classes

--- src/main/java/com/example/tuto/controller/ClientController.java
+++ src/main/java/com/example/tuto/controller/ClientController.java
@@ -1,6 +1,8 @@
 package com.example.tuto.controller;
 
+import com.example.tuto.dao.ClientDao;
 import com.example.tuto.entities.Client;
+import com.example.tuto.service.ClientService;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,9 +12,15 @@ import static org.springframework.util.MimeTypeUtils.APPLICATION_JSON_VALUE;
 @RequestMapping(value = "api/client")
 public class ClientController {
 
+    private ClientService clientService;
+
+    public ClientController(ClientService clientService) {
+        this.clientService = clientService;
+    }
+
     @ResponseStatus(value = HttpStatus.CREATED)
     @PostMapping(consumes = APPLICATION_JSON_VALUE)
     public void creer(@RequestBody Client client) {
-
+        this.clientService.creer(client);
     }
 }
--- src/main/java/com/example/tuto/service/ClientService.java
+++ src/main/java/com/example/tuto/service/ClientService.java
@@ -1,13 +1,21 @@
 package com.example.tuto.service;
 
+import com.example.tuto.dao.ClientDao;
 import com.example.tuto.entities.Client;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @Service
 public class ClientService {
 
-    public void creer(Client client) {
+    private ClientDao clientDao;
 
+    public ClientService(ClientDao clientDao) {
+        this.clientDao = clientDao;
+    }
+
+    public void creer(Client client) {
+        this.clientDao.save(client);
     }
 }
