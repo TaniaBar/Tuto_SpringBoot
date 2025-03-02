2633390740b256663c8e93372167098cdc392767
TaniaBar <tania.barillaro1@gmail.com>
2025-03-02 10:56:33 +0100

    clean code

--- src/main/java/com/example/tuto/entities/Sentiment.java
+++ src/main/java/com/example/tuto/entities/Sentiment.java
@@ -59,11 +59,4 @@ public class Sentiment {
         this.client = client;
     }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Long getId() {
-        return id;
-    }
 }
